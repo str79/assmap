e37e47c21184f40898565c52932e6a109d80cc7d
str79 <61847251+str79@users.noreply.github.com>
2020-08-09 17:02:54 +0300

    Add files via upload
    first copy

---
+++ settings.js
@@ -0,0 +1,896 @@
+var Profiles=[
+	{
+		'Name':'Флоренция',
+		'File':'fl.jpg',
+		'pointarr':'flPt',
+		'offsetTop':'-358px',
+		'offsetLeft':'-191px',
+		'StartIndex':1,
+		'zoom':1,
+		'GpoupList':'\
+		<div href="#" class="list-group-item"> \
+		<h4 class="list-group-item-heading"><span class="icon"></span><span class="text">&nbsp;Перья</span></h4> \
+		</div> \
+		<div href="#" class="list-group-item"> \
+		<h4 class="list-group-item-heading"> \
+		<span class="icon"></span><span class="text">&nbsp;Метки с глазом</span></h4> \
+		</div> \
+		<div href="#" class="list-group-item autohist"> \
+		<h4 class="list-group-item-heading"><span class="icon"></span><span class="text">&nbsp;История</span></h4> \
+		</div> \
+		',
+	},
+	{
+		'Name':'Вилла дяди Марио',
+		'File':'MonteJonny.jpg',
+		'pointarr':'MonteJonnyPt',
+		'offsetTop':'-189px',
+		'offsetLeft':'101px',
+		'StartIndex':1,
+		'zoom':0.8,
+		'GpoupList':'\
+		<div href="#" class="list-group-item"> \
+		<h4 class="list-group-item-heading"><span class="icon"></span><span class="text">&nbsp;Перья</span></h4> \
+		</div> \
+		<div href="#" class="list-group-item"> \
+		<h4 class="list-group-item-heading"> \
+		<span class="icon"></span><span class="text">&nbsp;Метки с глазом</span></h4> \
+		</div> \
+		<div href="#" class="list-group-item"> \
+		<h4 class="list-group-item-heading"> \
+		<span class="icon"></span><span class="text">&nbsp;Кувшины</span></h4> \
+		</div> \
+		<div href="#" class="list-group-item autohist"> \
+		<h4 class="list-group-item-heading"><span class="icon"></span><span class="text">&nbsp;История</span></h4> \
+		</div> \
+		',
+	},
+	{
+		'Name':'Тоскана',
+		'File':'toska_na.jpg',
+		'pointarr':'ToskanaPt',
+		'offsetTop':'-379px',
+		'offsetLeft':'-83px',
+		'StartIndex':1,
+		'zoom':1,
+		'GpoupList':'\
+		<div href="#" class="list-group-item"> \
+		<h4 class="list-group-item-heading"><span class="icon"></span><span class="text">&nbsp;Перья</span></h4> \
+		</div> \
+		<div href="#" class="list-group-item"> \
+		<h4 class="list-group-item-heading"> \
+		<span class="icon"></span><span class="text">&nbsp;Метки с глазом</span></h4> \
+		</div> \
+		<div href="#" class="list-group-item autohist"> \
+		<h4 class="list-group-item-heading"><span class="icon"></span><span class="text">&nbsp;История</span></h4> \
+		</div> \
+		',
+	},
+	{
+		'Name':'Форли',
+		'File':'forli.jpg',
+		'pointarr':'forliPt',
+		'offsetTop':'-933px',
+		'offsetLeft':'46px',
+		'StartIndex':1,
+		'zoom':0.8,
+		'GpoupList':'\
+		<div href="#" class="list-group-item"> \
+		<h4 class="list-group-item-heading"><span class="icon"></span><span class="text">&nbsp;Перья</span></h4> \
+		</div> \
+		<div href="#" class="list-group-item"> \
+		<h4 class="list-group-item-heading"> \
+		<span class="icon"></span><span class="text">&nbsp;Метки с глазом</span></h4> \
+		</div> \
+		<div href="#" class="list-group-item autohist"> \
+		<h4 class="list-group-item-heading"><span class="icon"></span><span class="text">&nbsp;История</span></h4> \
+		</div> \
+		',
+	},
+	{
+		'Name':'Венеция',
+		'File':'venezia.jpg',
+		'pointarr':'veneziaPt',
+		'offsetTop':'-545px',
+		'offsetLeft':'-739px',
+		'StartIndex':1,
+		'zoom':0.8,
+		'GpoupList':'\
+		<div href="#" class="list-group-item"> \
+		<h4 class="list-group-item-heading"><span class="icon"></span><span class="text">&nbsp;Перья</span></h4> \
+		</div> \
+		<div href="#" class="list-group-item"> \
+		<h4 class="list-group-item-heading"> \
+		<span class="icon"></span><span class="text">&nbsp;Метки с глазом</span></h4> \
+		</div> \
+		<div href="#" class="list-group-item autohist"> \
+		<h4 class="list-group-item-heading"><span class="icon"></span><span class="text">&nbsp;История</span></h4> \
+		</div> \
+		',
+	}
+];
+var flPt=[
+	{
+		'Name':'На перилах тюрьмы',
+		'CoordX':'1502px',
+		'CoordY':'967px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше где кран',
+		'CoordX':'754px',
+		'CoordY':'1108px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балке у реки',
+		'CoordX':'1310px',
+		'CoordY':'1224px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балке, между балконами',
+		'CoordX':'1173px',
+		'CoordY':'1263px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше, на дымоходе',
+		'CoordX':'1044px',
+		'CoordY':'1132px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше, рядом с башней',
+		'CoordX':'781px',
+		'CoordY':'1179px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На кране',
+		'CoordX':'809px',
+		'CoordY':'890px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Сбоку на окне',
+		'CoordX':'931px',
+		'CoordY':'695px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На самом верху, на кресте',
+		'CoordX':'1328px',
+		'CoordY':'696px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'За тюрьмой, на кране',
+		'CoordX':'1536px',
+		'CoordY':'974px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше метка с глазом',
+		'CoordX':'1862px',
+		'CoordY':'329px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На крыше, в развалинах дома',
+		'CoordX':'1756px',
+		'CoordY':'779px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Символ на крыше',
+		'CoordX':'1811px',
+		'CoordY':'872px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На стене дома, на балке',
+		'CoordX':'1856px',
+		'CoordY':'1165px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балке над ателье',
+		'CoordX':'1647px',
+		'CoordY':'1165px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Перо на крыше',
+		'CoordX':'1748px',
+		'CoordY':'883px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Над одной из дверей, с внешней стороны',
+		'CoordX':'1777px',
+		'CoordY':'275px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Глаз у куртизанок',
+		'CoordX':'1524px',
+		'CoordY':'513px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На окне дома где во дворе клад',
+		'CoordX':'1222px',
+		'CoordY':'562px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше, на осколке от дома',
+		'CoordX':'1362px',
+		'CoordY':'351px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене дома, на балке',
+		'CoordX':'1298px',
+		'CoordY':'201px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене (не в центре квадрата)',
+		'CoordX':'747px',
+		'CoordY':'708px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На столбе',
+		'CoordX':'700px',
+		'CoordY':'750px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Перо на балке между зданиями',
+		'CoordX':'773px',
+		'CoordY':'505px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Перо на балке на крыше',
+		'CoordX':'879px',
+		'CoordY':'396px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Знак у северной башни',
+		'CoordX':'852px',
+		'CoordY':'356px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На стене дома у реки, на балке',
+		'CoordX':'564px',
+		'CoordY':'1243px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На окне дома',
+		'CoordX':'596px',
+		'CoordY':'1043px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На окне храма',
+		'CoordX':'526px',
+		'CoordY':'834px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балках у стены',
+		'CoordX':'306px',
+		'CoordY':'482px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Около стены, на балке',
+		'CoordX':'735px',
+		'CoordY':'253px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На окне здания',
+		'CoordX':'676px',
+		'CoordY':'662px',
+		'Groups':'[0]',
+	},
+]
+var MonteJonnyPt=[	
+	{
+		'Name':'Штрих код',
+		'CoordX':'778px',
+		'CoordY':'649px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На стене здания, венера',
+		'CoordX':'650px',
+		'CoordY':'737px',
+		'Groups':'[2]',
+	},
+	{
+		'Name':'На стене, со стороны замка, марс ',
+		'CoordX':'746px',
+		'CoordY':'784px',
+		'Groups':'[2]',
+	},
+	{
+		'Name':'На стене, со стороны замка, плутон',
+		'CoordX':'993px',
+		'CoordY':'783px',
+		'Groups':'[2]',
+	},
+	{
+		'Name':'Во дворе дома, нептун',
+		'CoordX':'1018px',
+		'CoordY':'964px',
+		'Groups':'[2]',
+	},
+	{
+		'Name':'На стене, на окне, юпитер',
+		'CoordX':'1107px',
+		'CoordY':'1051px',
+		'Groups':'[2]',
+	},
+	{
+		'Name':'На полукруглой крыше, миневра',
+		'CoordX':'969px',
+		'CoordY':'1118px',
+		'Groups':'[2]',
+	},
+	{
+		'Name':'На окне, на балках, диана',
+		'CoordX':'778px',
+		'CoordY':'1099px',
+		'Groups':'[2]',
+	},
+	{
+		'Name':'На стене дома, аполлон',
+		'CoordX':'632px',
+		'CoordY':'1053px',
+		'Groups':'[2]',
+	},
+	{
+		'Name':'На крыше',
+		'CoordX':'768px',
+		'CoordY':'958px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше',
+		'CoordX':'120px',
+		'CoordY':'882px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше',
+		'CoordX':'1295px',
+		'CoordY':'146px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балке с внутренней стороны',
+		'CoordX':'802px',
+		'CoordY':'1178px',
+		'Groups':'[0]',
+	},
+]	
+
+var ToskanaPt=[
+	{
+		'Name':'Где смотровая башня, на балке',
+		'CoordX':'632px',
+		'CoordY':'903px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Внизу знак',
+		'CoordX':'696px',
+		'CoordY':'872px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'Где смотровая башня, наверху',
+		'CoordX':'799px',
+		'CoordY':'677px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'Где смотровая башня, на балке',
+		'CoordX':'641px',
+		'CoordY':'656px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Где смотровая башня, на крыше',
+		'CoordX':'712px',
+		'CoordY':'760px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Между башнями, у крыш домов',
+		'CoordX':'824px',
+		'CoordY':'765px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На балки, на стене башни',
+		'CoordX':'823px',
+		'CoordY':'739px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На смотровой башне, на балке',
+		'CoordX':'952px',
+		'CoordY':'567px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше какой-то развалины',
+		'CoordX':'930px',
+		'CoordY':'1069px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене башни, на балке',
+		'CoordX':'720px',
+		'CoordY':'1145px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене, на крыше какой-то сторожки',
+		'CoordX':'749px',
+		'CoordY':'1301px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене монастыря, не на самом верху',
+		'CoordX':'1572px',
+		'CoordY':'1933px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На крыше, перо',
+		'CoordX':'1573px',
+		'CoordY':'1872px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На смотровой башне, на балке',
+		'CoordX':'1239px',
+		'CoordY':'14px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Знак внутри стен',
+		'CoordX':'569px',
+		'CoordY':'1247px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На колонне',
+		'CoordX':'413px',
+		'CoordY':'1243px',
+		'Groups':'[0]',
+	},
+]
+var forliPt=[
+	{
+		'Name':'На стене обзорной башни',
+		'CoordX':'1175px',
+		'CoordY':'1419px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Перо на шесте, в воде',
+		'CoordX':'1335px',
+		'CoordY':'765px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене',
+		'CoordX':'1471px',
+		'CoordY':'76px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На стене дома, на балке',
+		'CoordX':'1277px',
+		'CoordY':'254px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене церкви, на балке',
+		'CoordX':'241px',
+		'CoordY':'447px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше',
+		'CoordX':'569px',
+		'CoordY':'1113px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене, на балке',
+		'CoordX':'874px',
+		'CoordY':'1157px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На круглой крыше',
+		'CoordX':'700px',
+		'CoordY':'1524px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше, на балке',
+		'CoordX':'655px',
+		'CoordY':'1360px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене квадратного сквера',
+		'CoordX':'590px',
+		'CoordY':'1315px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'Между зданиями, на балках',
+		'CoordX':'518px',
+		'CoordY':'1339px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На круглой крыше',
+		'CoordX':'393px',
+		'CoordY':'1344px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене дома, на балке',
+		'CoordX':'398px',
+		'CoordY':'1533px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене здания, на балке',
+		'CoordX':'508px',
+		'CoordY':'1730px',
+		'Groups':'[0]',
+	},
+]
+var veneziaPt=[
+	{
+		'Name':'На балке в миссии знакомства с ворами',
+		'CoordX':'399px',
+		'CoordY':'1377px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Между домами',
+		'CoordX':'1281px',
+		'CoordY':'1773px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше',
+		'CoordX':'482px',
+		'CoordY':'1258px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На балках',
+		'CoordX':'1054px',
+		'CoordY':'1498px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балках',
+		'CoordX':'1734px',
+		'CoordY':'1303px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше, где колокол',
+		'CoordX':'2126px',
+		'CoordY':'1094px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На крыше, под колоколом',
+		'CoordX':'2652px',
+		'CoordY':'1938px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На реке, на шесте',
+		'CoordX':'2498px',
+		'CoordY':'1519px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балконе',
+		'CoordX':'2728px',
+		'CoordY':'1478px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене дома, на балке.',
+		'CoordX':'1816px',
+		'CoordY':'1957px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене здания на балке, над рекой.',
+		'CoordX':'2155px',
+		'CoordY':'1748px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балконе',
+		'CoordX':'2419px',
+		'CoordY':'1884px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балке, почти где крыша.',
+		'CoordX':'2598px',
+		'CoordY':'2036px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше',
+		'CoordX':'2693px',
+		'CoordY':'1677px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На балке, на стене',
+		'CoordX':'2890px',
+		'CoordY':'1604px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балках, в подворотне',
+		'CoordX':'2602px',
+		'CoordY':'1090px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше, на кране',
+		'CoordX':'2894px',
+		'CoordY':'1748px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балках, между домами.',
+		'CoordX':'1182px',
+		'CoordY':'2076px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене дома',
+		'CoordX':'1368px',
+		'CoordY':'2208px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балке',
+		'CoordX':'642px',
+		'CoordY':'1905px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Перо на балках',
+		'CoordX':'838px',
+		'CoordY':'1619px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балках, над мостом.',
+		'CoordX':'3560px',
+		'CoordY':'1914px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На мачте корабля, не на самом верху',
+		'CoordX':'1405px',
+		'CoordY':'2530px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балке, на стене дома',
+		'CoordX':'1640px',
+		'CoordY':'2486px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Где карнавал, на балке, на стене',
+		'CoordX':'2119px',
+		'CoordY':'2283px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балке, на стене дома',
+		'CoordX':'478px',
+		'CoordY':'1885px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше дома, на балке',
+		'CoordX':'222px',
+		'CoordY':'1705px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Во дворе, на балках, над колодцем',
+		'CoordX':'627px',
+		'CoordY':'1410px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Между домами, на балках',
+		'CoordX':'882px',
+		'CoordY':'1228px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'У воды, на шесте.',
+		'CoordX':'1276px',
+		'CoordY':'1202px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Перо на балке, на стене дома',
+		'CoordX':'1984px',
+		'CoordY':'1051px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На мосту, на шпиле',
+		'CoordX':'2296px',
+		'CoordY':'1129px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Под мостом',
+		'CoordX':'2414px',
+		'CoordY':'1193px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На балках, на стене.',
+		'CoordX':'3995px',
+		'CoordY':'1704px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балке у дома',
+		'CoordX':'3511px',
+		'CoordY':'1699px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене, на балке',
+		'CoordX':'3580px',
+		'CoordY':'1028px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене, на другой балке',
+		'CoordX':'3809px',
+		'CoordY':'1049px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене дома, на балке',
+		'CoordX':'3209px',
+		'CoordY':'1249px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше',
+		'CoordX':'2919px',
+		'CoordY':'1161px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На крыше дома, на балке',
+		'CoordX':'3288px',
+		'CoordY':'1460px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше штаба, на балке.',
+		'CoordX':'3319px',
+		'CoordY':'2095px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балке, напротив церкви',
+		'CoordX':'4129px',
+		'CoordY':'2219px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На крыше церкви',
+		'CoordX':'4080px',
+		'CoordY':'2190px',
+		'Groups':'[1]',
+	},
+	{
+		'Name':'На балках, почти где крыша',
+		'CoordX':'3833px',
+		'CoordY':'2063px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене дома, на балке',
+		'CoordX':'1628px',
+		'CoordY':'168px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Между домами, на балке',
+		'CoordX':'1343px',
+		'CoordY':'379px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На балконе дома, на палке',
+		'CoordX':'1431px',
+		'CoordY':'574px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Крыша дома, на кране',
+		'CoordX':'1811px',
+		'CoordY':'497px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'В переулке, на балке',
+		'CoordX':'2084px',
+		'CoordY':'332px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене дома, на балке',
+		'CoordX':'2139px',
+		'CoordY':'184px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На стене дома, на балке',
+		'CoordX':'2040px',
+		'CoordY':'579px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'На шесте, у воды',
+		'CoordX':'2174px',
+		'CoordY':'582px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Над водой, на балке',
+		'CoordX':'2642px',
+		'CoordY':'971px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Перо на крыше дома, на балках',
+		'CoordX':'3568px',
+		'CoordY':'1086px',
+		'Groups':'[0]',
+	},
+]
